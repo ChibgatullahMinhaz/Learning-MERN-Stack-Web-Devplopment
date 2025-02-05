78fbea238795b61db8a667aab4d750a73156e0b9
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-05 23:54:15 +0600

    javaScript task 4 done

---
+++ JavaScript/task4/task4.js
@@ -0,0 +1,14 @@
+let a = isNaN("11");
+let ab = isNaN("kjhkj");
+console.log(a, ab);
+/**
+ * isNaN("11") of output is will be false cause it's
+ * "11" is a number; when value is not nunber like " kola , abcd etc" then will be
+ *  return true   
+ */
+let b = isNaN(2-10);
+console.log(parseInt(-5))
+console.log(b);
+/**
+ * this progame output is false . cause is this a negitive number.
+ */
