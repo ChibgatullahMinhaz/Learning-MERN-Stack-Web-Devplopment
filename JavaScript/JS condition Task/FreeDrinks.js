a15932ddf2cc2d391d92b9bdede08ca918fbb28d
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-06 02:03:11 +0600

    added comment

--- JavaScript/JS condition Task/FreeDrinks.js
+++ JavaScript/JS condition Task/FreeDrinks.js
@@ -1,3 +1,10 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
 let burger = 400;
 
 if(burger >= 500){
--- JavaScript/JS condition Task/gradeSwitch.js
+++ JavaScript/JS condition Task/gradeSwitch.js
@@ -1,5 +1,19 @@
-let mark = 33;
+/***
 
+Grade Calculator
+
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
+
+
+let mark = 33;
 switch (true) {
     case mark < 33:
         console.log("Faill")
