add47b854a717828a636f2c820ca7b9275883464
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-05 23:29:21 +0600

    added js task one

---
+++ JavaScript/task1/index.js
@@ -0,0 +1,7 @@
+let isHave = 5000;
+let orang = 300;
+let apple = 500;
+let frutPrice = orang + apple ;
+console.log(frutPrice)
+let returnPrice = isHave - frutPrice;
+console.log( "shop keeper will retun: " + returnPrice + " Tk") 
