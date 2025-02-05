3c98523a974edfe841f4cd1f7a124aa05fb2e09b
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-06 00:38:50 +0600

    grade system usng if-esle condition

---
+++ JavaScript/JS condition Task/gradeIf-else.js
@@ -0,0 +1,23 @@
+let mark = 60;
+
+if (mark > 30 && mark <= 33) {
+    console.log("Pass")
+}
+else if (mark >= 40 && mark <= 49) {
+    console.log("Grade c")
+}
+else if (mark >= 50 && mark <= 59) {
+    console.log("Grade B")
+} 
+else if (mark >= 60 && mark <= 69) {
+    console.log("Grade A-")
+} 
+else if (mark >= 70 && mark <= 79) {
+    console.log("Grade A")
+} 
+else if (mark >= 80 && mark <= 100) {
+    console.log("Grade A+")
+} 
+else {
+    console.log("faill , your need more study")
+}
