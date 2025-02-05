7f7f85edfa43598e855ea709d4fe3bb76dc7bd83
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-05 23:37:17 +0600

    task2 done

---
+++ JavaScript/task2/avg.js
@@ -0,0 +1,9 @@
+const math = 90;
+const physics = 97;
+const chemistry = 95;
+const Biology = 94;
+const bangla = 80;
+let totalMark = math + physics + chemistry + Biology + bangla;
+console.log(totalMark)
+let avarage = totalMark / 5;
+console.log(avarage)
