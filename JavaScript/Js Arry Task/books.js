faf10a06bfba0d369053ffcb6aacc4095af4a68f
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-08 23:28:41 +0600

    js array task 3

---
+++ JavaScript/Js Arry Task/books.js
@@ -0,0 +1,9 @@
+let books = ["The Alchemist", "The Monk Who Sold His Ferrari", "The Power of Positive Thinking", "The Secret"];
+
+console.log(books.length) ;
+
+if(books.includes("The Secret")){
+    console.log("The Secret is available in the list");
+}else{
+    console.log("The Secret is not available in the list");
+}
