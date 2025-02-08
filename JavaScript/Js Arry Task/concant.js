821229bd9c836646cb349b5c9e919ebba435d6dd
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-08 23:35:34 +0600

    concat two array

---
+++ JavaScript/Js Arry Task/concant.js
@@ -0,0 +1,6 @@
+let frouts = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"];
+console.log(frouts);
+let animals = ["Lion", "Tiger", "Elephant", "Deer", "Zebra"];
+console.log(animals);
+let concant = frouts.concat(animals);
+console.log(concant)
