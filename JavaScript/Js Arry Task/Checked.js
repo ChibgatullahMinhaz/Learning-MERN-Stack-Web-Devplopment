6d7816203143ec35dec8269b57766b7ad333278d
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-08 23:32:27 +0600

    js array task 4

---
+++ JavaScript/Js Arry Task/Checked.js
@@ -0,0 +1,12 @@
+const frouts = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"];
+
+const naem = [];
+
+const name = "jambura";
+
+if (Array.isArray(name)) {
+    console.log("it's an array");
+    
+} else {
+    console.log("it's not an array");
+}
