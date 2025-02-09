c27cb4078afbbcc2abecd34c8dfa7bfcf980f0e5
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-10 02:30:26 +0600

    coundown

---
+++ JavaScript/js loop task/for-Loop/coundown.js
@@ -0,0 +1,3 @@
+for (let index = 81; index >=65; index--) {
+    console.log(index);
+}
--- JavaScript/js loop task/for-Loop/multiplication-table.js
+++ JavaScript/js loop task/for-Loop/multiplication-table.js
@@ -1,6 +1,11 @@
 let num = 9; 
  for (let i = 1; i <=10; i++) {
-    console.log( num, " * ", i, " = ", num*i)
+    console.log(`${num} * ${i} = ${num * i}`);
  }
 
- 
+//  onother way
+ for (let index = 0; index <=10; index++) {
+    const result = num + " * "+ index +" = "+ num * index;
+    console.log(result);
+    
+ }
