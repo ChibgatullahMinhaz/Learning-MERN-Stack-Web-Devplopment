d734b9176b4dea03df397b75f09bbf0a28a1dc82
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-10 02:14:28 +0600

    sum of odd and even number

---
+++ JavaScript/js loop task/for-Loop/sum.js
@@ -0,0 +1,16 @@
+// odd number 
+let oddSum = 0;
+for (let i = 91; i <=129 ; i++) {
+    if ( i % 2 !== 0) {
+        oddSum += i;
+    }    
+    }
+    console.log(oddSum);
+    // even number 
+    let sum = 0;
+    for (let i = 51; i <=85 ; i++) {
+    if ( i % 2 == 0) {
+        sum += i;
+    }    
+    }
+    console.log(sum);
