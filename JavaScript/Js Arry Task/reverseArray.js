6d915a03b66e62923f59da37306768a7e7539695
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-11 20:23:53 +0600

    array reverse

--- JavaScript/Js Arry Task/reverseArray.js
+++ JavaScript/Js Arry Task/reverseArray.js
@@ -8,3 +8,17 @@ for (let i = 0 ; i < str.length; i++){
     rev_str.unshift(str[i]);
 }
 console.log(rev_str); 
+
+// reverse ontther way
+
+let rev_str2 = [];
+
+for(let i = str.length -1; i >=0; i--){
+    rev_str2.push(str[i]);
+}
+console.log(rev_str2);
+
+// using reverse functhin 
+
+let rev_str3 = str.reverse();
+console.log(rev_str3);
