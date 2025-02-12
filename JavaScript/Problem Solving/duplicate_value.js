fea6b2c5f2d1e26143bdcc51335e31cbf2e0e582
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-13 01:52:40 +0600

    finde duplicate value

---
+++ JavaScript/Problem Solving/duplicate_value.js
@@ -0,0 +1,25 @@
+let number = [2, 3, 2, 2, 3, 4, 5, 76, 7, 2, 3, 5, 76, 55, 55];
+
+function uniqe(arr) {
+    let uniqeNumber = [];
+    for (const num of arr) {
+        if (uniqeNumber.includes(num)) {
+
+        } else {
+            uniqeNumber.push(num)
+        }
+    }
+    return uniqeNumber;
+}
+console.log(uniqe(number))
+// onotehr way
+function uniqe(arr) {
+    let uniqeNumber = [];
+    for (const num of arr) {
+        if (!uniqeNumber.includes(num)) {
+            uniqeNumber.push(num)
+        }
+    }
+    return uniqeNumber;
+}
+console.log(uniqe(number))
