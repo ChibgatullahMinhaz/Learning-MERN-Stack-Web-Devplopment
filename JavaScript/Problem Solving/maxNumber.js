2550781587b10487465bd2b17071b00c9debda69
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-13 02:20:24 +0600

    finde max number onther way

--- JavaScript/Problem Solving/maxNumber.js
+++ JavaScript/Problem Solving/maxNumber.js
@@ -11,3 +11,22 @@ function max(number) {
     return max;
 }
 console.log(max(numbers))
+// onther way
+let numbers2 = [2, 3, 2, 2, 3, 4, 5, 76, 7, 2, 3, 5, 76, 55, 55];
+
+function maxNumber(params) {
+    let maximun = Math.max(...params);
+    return maximun;
+}
+const result = maxNumber(numbers2);
+console.log(result)
+
+// other way 
+function maxNumberOfArray(params) {
+    let maxNumber = params[0];
+    for (const number of params) {
+        maxNumber = Math.max(maxNumber, number)
+    }
+    return maxNumber;
+}
+console.log(maxNumberOfArray(numbers))
