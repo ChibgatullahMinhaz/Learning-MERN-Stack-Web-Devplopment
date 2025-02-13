ce54f7df0c7e4334360dab18cc1e4d2b95b27a85
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-14 03:21:19 +0600

    finde a value

---
+++ JavaScript/js-problems-part1-practice-tasks/finding.js
@@ -0,0 +1,16 @@
+const  numbers = [5,6,11,12,98,5];
+const friends = ["minhaz", "john", "doe", "jane", "smith"];
+
+function findValue(values, inputValue) {
+   let count = 0;
+   let message = "";
+   for (const value of values) {
+    if (value === inputValue) {
+        message = "it's here bro "
+        count++
+    }
+   }
+   return `Times of: ${count} Message:  ${message}`;
+}
+const finde = findValue(numbers , 5);
+console.log(finde);
