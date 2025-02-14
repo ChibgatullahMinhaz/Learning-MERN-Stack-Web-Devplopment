913b1f99ba0271a239717da64942d19f0a43b202
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-14 16:14:18 +0600

    onther tecnique

--- JavaScript/js-problems-part2-practice-tasks/lowest.js
+++ JavaScript/js-problems-part2-practice-tasks/lowest.js
@@ -1,7 +1,7 @@
 const heights2 = [167, 190, 120, 165, 137,8,9,900,1];
 
 function lowestNumber(Data) {
-    let lowest = heights2[0];
+    let lowest = Data[0];
     for (const num of Data) {
         if (lowest > num) {
             lowest = num
@@ -9,6 +9,15 @@ function lowestNumber(Data) {
     }
     return lowest
 }
-setInterval(() => {
-    console.log( lowestNumber(heights2))
-}, 1000);
+// setInterval(() => {
+//     // console.log( lowestNumber(heights2))
+// }, 1000);
+
+// onther way
+const heights = [167, 190, 120, 165, 137,8,9,900,1];
+
+function findeLowest(data){
+    const lowest = Math.min(...data);
+    return lowest
+}
+console.log(findeLowest(heights));
