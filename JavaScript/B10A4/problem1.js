024b5aa223a7d5df933fb7e3bb30f149051e4e0b
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-15 15:37:21 +0600

    tax calculate

--- JavaScript/B10A4/problem1.js
+++ JavaScript/B10A4/problem1.js
@@ -1,9 +1,9 @@
 function calculateTax(income, cost) {
-    if (Number.isInteger(income) !== true || Number.isInteger(cost) !== true) {
-        return "not valid "
+    if (Number.isInteger(income) !== true || Number.isInteger(cost) !== true || income < 0 || cost < 0) {
+        return "Invalid Input "
     }
     const paybleAmount = ((income - cost) * 20) / 100;
     return paybleAmount;
 }
-const result = calculateTax(10000, 3000);
+const result = calculateTax(10000, 9000);
 console.log(result)
