e73ee20a1b6ea8b708ccc6863905adc3810619b0
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-16 19:32:49 +0600

    bazar hisab calculator

---
+++ JavaScript/Problem Solving/concept/Bazar.js
@@ -0,0 +1,11 @@
+const bazar = {
+    Budget: 500,
+    Fish: 200,
+    GreenChili: 100,
+    Vagitales: 50
+}
+
+function bazarHisab(input ) {
+    console.log(input)
+}
+const output = bazarHisab(bazar)
