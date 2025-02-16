6e56e2dca6e6509199fc15ee7907441e489c61c3
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-16 19:54:22 +0600

    bazare hisab calculator

--- JavaScript/Problem Solving/concept/Bazar.js
+++ JavaScript/Problem Solving/concept/Bazar.js
@@ -1,11 +1,23 @@
 const bazar = {
     Budget: 500,
-    Fish: 200,
-    GreenChili: 100,
-    Vagitales: 50
+    Cost: {
+        Fish: 200,
+        GreenChili: 100,
+        Vagitales: 50,
+        donepata: 50
+    }
 }
 
-function bazarHisab(input ) {
-    console.log(input)
+function bazarHisab(input) {
+    if (typeof input !== "object" || Array.isArray(input) || !input.Cost) {
+        return "invalid Input";
+    }
+    let totalCost = 0;
+    for (const cost of Object.values(input.Cost)) {
+        totalCost = totalCost + cost;
+    }
+  const totalRemaining = input.Budget - totalCost;
+  return totalRemaining;
 }
 const output = bazarHisab(bazar)
+console.log(output)
