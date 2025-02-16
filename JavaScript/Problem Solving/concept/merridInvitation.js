c0be300e4208eeb0ffbbaaf5088179fe417b4271
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-17 00:08:19 +0600

    Reletives dawa list

---
+++ JavaScript/Problem Solving/concept/merridInvitation.js
@@ -0,0 +1,31 @@
+const relatives = [
+    { name: "Alice", member: 1 },
+    { name: "Bob", member: 2 },
+    { name: "Charlie", member: 3 },
+    { name: "Alice", member: 1 },
+    { name: "David", member: 4 },
+    { name: "Eve", member: 5 },
+    { name: "Bob", member: 2 },
+    { name: "Frank", member: 6 },
+    { name: "Grace", member: 7 },
+    { name: "Alice", member: 1 }
+];
+function DawahList(ShadiList) {
+    let uniqeList = [];
+    let totalMember = 0;
+    for (const list of ShadiList) {
+        let isUnique = true;
+        for (const newRelative of uniqeList) {
+            if (list.name === newRelative.name && list.member === newRelative.member) {
+                isUnique = false;
+            }
+        }
+        if (isUnique) {
+            uniqeList.push(list)
+            totalMember += list.member;
+        }
+    }
+    return [uniqeList , totalMember];
+}
+const output = DawahList(relatives);
+console.log(output)
