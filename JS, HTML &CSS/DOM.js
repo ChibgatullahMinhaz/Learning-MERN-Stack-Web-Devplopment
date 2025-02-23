5ee995dc63e8a19f80e5e9f2a7656b73c2e84132
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-23 20:25:12 +0600

    dome practice

--- JS, HTML &CSS/DOM.js
+++ JS, HTML &CSS/DOM.js
@@ -1,4 +1,15 @@
 const cliked = document.getElementById("click");
 cliked.addEventListener('click', ()=>{
-    cliked.innerText= 'ON'
+    document.body.style.backgroundColor = 'red'
+})
+
+function  makeRed(event) {
+    document.body.style.backgroundColor = 'red'
+}
+function makeBlue (event){
+    document.body.style.backgroundColor = "blue"
+}
+document.getElementById('makeSkay').addEventListener('click', 
+    function makeSkay (event) {
+  document.body.style.backgroundColor = 'skyblue'  
 })
