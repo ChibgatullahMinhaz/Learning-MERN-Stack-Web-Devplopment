bf37915c01b32a518635e94a20687e80428d97e6
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-23 23:50:28 +0600

    set dynamic name

--- JS, HTML &CSS/DOM.js
+++ JS, HTML &CSS/DOM.js
@@ -1,15 +1,30 @@
 const cliked = document.getElementById("click");
-cliked.addEventListener('click', ()=>{
+cliked.addEventListener('click', () => {
     document.body.style.backgroundColor = 'red'
 })
 
-function  makeRed(event) {
+function makeRed(event) {
     document.body.style.backgroundColor = 'red'
 }
-function makeBlue (event){
+function makeBlue(event) {
     document.body.style.backgroundColor = "blue"
 }
-document.getElementById('makeSkay').addEventListener('click', 
-    function makeSkay (event) {
-  document.body.style.backgroundColor = 'skyblue'  
-})
+document.getElementById('makeSkay').addEventListener('click',
+    function makeSkay(event) {
+        document.body.style.backgroundColor = 'skyblue'
+        document.body.innerHTML = `
+ <div className="divider">Default</div>
+  `
+    });
+function updateNAme() {
+
+    const updateValue = document.getElementById('updatebtn');
+
+    updateValue.addEventListener('click', function
+        (event) {
+        const tile = document.getElementById('yourName');
+        const titileValue = document.getElementById('nameValue').value;
+        tile.innerText = titileValue;
+    })
+}
+updateNAme()
