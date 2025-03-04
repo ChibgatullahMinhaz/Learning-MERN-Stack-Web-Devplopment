eaece2ad862acb6f9f0848811e084f5dc79b09dd
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-04 23:27:58 +0600

    defualt paremeter

---
+++ JS, HTML &CSS/defualtParemeter.js
@@ -0,0 +1,9 @@
+function sum(num1 = 0, num2 = 0){
+    return num1 + num2
+}
+console.log(sum(20,30));
+// multifly
+function multifly(num1=1, num2=1) {
+    return num1 * num2
+}
+console.log(multifly(10,10));
