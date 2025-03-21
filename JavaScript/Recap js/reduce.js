3b451ed3a89a43363e817af491a27e3c2fadba63
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-22 02:13:09 +0600

    reduce method

--- JavaScript/Recap js/reduce.js
+++ JavaScript/Recap js/reduce.js
@@ -7,5 +7,16 @@ const result = numbers.reduce((accumulator, currentValue) => {
   return accumulator * currentValue;
 }, 1);
 
-console.log(result.toFixed()); 
+// console.log(result.toFixed()); 
 
+const items = [
+    { name: "Apple", price: 100 },
+    { name: "Banana", price: 200 },
+    { name: "Orange", price: 300 },
+  ];
+  
+  const totalPrice = items.reduce((accumulator, currentValue) => {
+      console.log(accumulator);
+ return    accumulator + currentValue.price;
+  }, 0);
+  
