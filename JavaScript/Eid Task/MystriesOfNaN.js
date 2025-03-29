9753e5d94465ea7c6323988cef7bd4008ec9fc75
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-30 00:52:49 +0600

    mystries of NaN

---
+++ JavaScript/Eid Task/MystriesOfNaN.js
@@ -0,0 +1,12 @@
+console.log( NaN === 2);
+console.log( NaN === NaN);
+// output is : False , cause its cannot equal of anything so gives False. here are equal data types but not equal Data or values .
+console.log( typeof NaN) ; // NaN datatype is  Number but is not a legal number 
+console.log( typeof isNaN()); // isNaN() return a boolean value, cause its checked 
+console.log(NaN !== NaN); // output is true, cause in javaScript , NaN cannot equal of others . here condition is Nan is not Equal Nan its true.
+console.log(typeof NaN === typeof NaN); // in js , NaN cannot equal it self but NaN Data type are equals. cause NaN data type is Number type which is not legal number.
+console.log(NaN === NaN); // in js , NaN cannot equal itself. so output is false. 
+
+console.log(NaN > 2);
+
+console.log(NaN < 2);
